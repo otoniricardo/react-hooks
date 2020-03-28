313e410a65c76f1af749b9fc227f2af439b6942e
otoniricardo <otoniricardo@gmail.com>
2020-03-28 11:37:39 -0300

    implementa useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [techs, setTechs] = useState(['ReactJS', 'NodeJS']);
+  const [techs, setTechs] = useState([]);
   const [newTech, setNewTech] = useState('');
 
   function handleAdd() {
     setTechs([...techs, newTech]);
     setNewTech('');
   }
+
+  useEffect(() => {
+    const storageTechs = localStorage.getItem('techs');
+    if (techs) setTechs(JSON.parse(storageTechs));
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('techs', JSON.stringify(techs));
+  }, [techs]);
 
   return (
     <>
